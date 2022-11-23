b7f340b9f1f22dc6991b80116b3aca949bb813ed
LucasMartines01 <lucas.martines@sptech.school>
2022-11-22 23:09:10 -0300

    Script final

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -22,10 +22,12 @@ idQuiz int primary key auto_increment,
 fkUsuario int, foreign key (fkusuario) references usuario(id),
 nota int);
 
+select * from usuario;
+
 select * from formulario;
 
 select * from quiz;
 
 select count(idQuiz), max(nota), round(avg(nota),0), min(nota) from quiz;
 
-select usuario.nome, quiz.nota from usuario join quiz on fkUsuario = id  where quiz.nota = (select max(nota) from quiz) group by usuario.nome order by nota  desc limit 5; 
+select usuario.nome, quiz.nota from usuario join quiz on fkUsuario = id  where quiz.nota = (select max(nota) from quiz) group by usuario.nome order by nota  desc limit 5;
